17aa3c851991544a08803d125fcdff30a79f1746
Melendez99 <melodygeorge99@gmail.com>
2023-11-12 18:26:23 -0800

    updated header file with parameters

--- george.h
+++ george.h
@@ -12,4 +12,14 @@
 #include <fcntl.h>
 #include <errno.h>
 
+/* interactive.c */
+int is_shell_interactive(info_t *shellInfo);
+int is_character_delimiter(char c, const char *delimiters);
+int is_alpha_character(int c);
+int string_to_integer(char *str);
+
+/* shelltokens.c */
+char **split_string_by_delim(char *inputStr, char delimiter);
+char **split_string_ignore_repeat_delim(char *inputStr, char *delimiters);
+
 #endif
